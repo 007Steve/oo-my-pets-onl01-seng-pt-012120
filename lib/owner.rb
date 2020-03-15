1860448264fcd271244e1128af8d2ec3d7a27735
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-03-15 15:08:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+ 
+ attr_accessor :name ,:species 
+ 
+ @@all = []
+ def initialize(name ="name")
+   @name = name
+   @species = species
+   @all << self
+ end
+ 
+ 
+
+def species()
+  
+end
+
+def say_species()
+   "I am  a #{@species}."
+end
+
+ def self.all
+    @@all
+  end
+  
+def self.reset_all
+  @@all.clear
+end
+
+def self.count
+  @@all.size
+end
+
+
 end
